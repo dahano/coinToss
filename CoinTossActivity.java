da78475959f27a00143a91a71bcc16c107524e89
ofir dahan <ofirdahan@live.com>
2016-08-22 22:46:41 -0700

    added background color

--- CoinTossActivity.java
+++ CoinTossActivity.java
@@ -1,9 +1,11 @@
 package ofirdahan.com.coinflip;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -13,6 +15,8 @@ public class CoinTossActivity extends AppCompatActivity {
     private TextView mHeadsview;
     private TextView mTailsview;
     private Button mFlipButton;
+    private RelativeLayout mRelativeLayout;
+
 
 
 
@@ -27,13 +31,14 @@ public class CoinTossActivity extends AppCompatActivity {
         mHeadsview = (TextView) findViewById(R.id.heads);
         mTailsview = (TextView) findViewById(R.id.tails);
         mFlipButton = (Button) findViewById(R.id.button);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relative);
 
         mHeadsview.setText("");
 
         View.OnClickListener listener = new View.OnClickListener() {
             Random randomFlip = new Random();
             int result;
-            
+
             @Override
             public void onClick(View v) {
                 // The button was clicked so Hide either the Heads or Tails
@@ -50,11 +55,14 @@ public class CoinTossActivity extends AppCompatActivity {
             private void enableTails() {
                 mHeadsview.setText("");
                 mTailsview.setText("Tails");
+                mRelativeLayout.setBackgroundColor(Color.RED);
             }
 
             private void enableHeads() {
                 mHeadsview.setText("Heads");
                 mTailsview.setText("");
+                mRelativeLayout.setBackgroundColor(Color.CYAN);
+
             }
         };
 
